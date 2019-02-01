194158dade26416ea2eb3de5a1ec7e867e8d8cc1
prateek951 <prince.cenation@gmail.com>
2019-02-02 00:00:22 +0530

    redux setup

--- skeleton/frontend/src/components/App.js
+++ skeleton/frontend/src/components/App.js
@@ -1,16 +1,21 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import Header from "./pages/Header";
 import Dashboard from "./pages/Dashboard";
+import store from "../store";
+
 class App extends React.Component {
   render() {
     return (
-      <React.Fragment>
-        <Header />
-        <div className="container">
-          <Dashboard />
-        </div>
-      </React.Fragment>
+      <Provider store={store}>
+        <React.Fragment>
+          <Header />
+          <div className="container">
+            <Dashboard />
+          </div>
+        </React.Fragment>
+      </Provider>
     );
   }
 }
